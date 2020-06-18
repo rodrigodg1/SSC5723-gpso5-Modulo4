916c43846a22bc5583843256703401c15a5cfaf0
rodrigog10 <62249997+rodrigog10@users.noreply.github.com>
2020-06-18 03:28:36 -0300

    Update destruir-arquivos.sh

--- destruir-arquivos.sh
+++ destruir-arquivos.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 
-
+#FUNÇÃO RESPONSAVEL POR DESTRUIR O ARQUIVO QUE RECEBE ($1) DO LOOP
 destroi_arquivo() {
 
    sleep 2
    
    #time -p rm arquivo$1.txt
+   #stace -c por possuir mais casas decimais
    strace -c rm arquivo$1.txt
    
 }
@@ -16,14 +17,17 @@ destroi_arquivo() {
 a=1
 
 
-#LOOP PARA AS 10 ITERACOES PARA REMOCAO
+#LOOP PARA A REMOÇÃO DOS ARQUIVOS
 while [ "$a" -lt 11 ]   
 do
    
 
    
    echo "\n\nDestruindo arquivo ($a/10)  ..."		
+   
+   #CHAMA A FUNÇÃO PASSANDO O INDICE DO ARQUIVO QUE FOI CRIADO ANTERIORMENTE
    destroi_arquivo $a
+   
    echo "Arquivo $a destruido !"
 
 

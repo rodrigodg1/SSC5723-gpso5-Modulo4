26d78c0d14aeba6458c47a1dd9b4953e8835229b
rodrigog10 <62249997+rodrigog10@users.noreply.github.com>
2020-06-19 11:14:49 -0300

    Update criar-arquivos.sh

--- criar-arquivos.sh
+++ criar-arquivos.sh
@@ -4,9 +4,12 @@
 #FUNÇÃO RESPONSÁVEL POR CRIAR O ARQUIVO DO TAMANHO QUE RECEBE EM $1
 cria_arquivo() {
 
+		
+   echo "Diretorio sendo utilizado para CRIACAO: $3"	
    sleep 2
    
-   dd if=/dev/zero of=arquivo$2.txt bs=$1 count=1 status=progress
+   #CRIA O ARQUIVO
+   dd if=/dev/zero of=$3/arquivo$2.txt bs=$1 count=1 status=progress
    
    
 }
@@ -19,22 +22,29 @@ a=1
 \n\nExemplo:1MB [ENTER]:"
    read tamanho
 
+   echo "\nDigite o Caminho do Sistema de Arquivo Destino"
+   echo "Exemplo: /media/rodrigo/FAT32/"
+   echo "ou"
+   echo ". para o Diretorio Atual"
+   read caminho
+
+
+
+
 #LOOP PARA CRIAR 10 ARQUIVOS PARA TIRAR A MÉDIA DOS TEMPOS 
 while [ "$a" -lt 11 ]   
 do
    
-
    
    echo "\n\nCriando arquivo ($a/10)  ..."
    
    #PASSA PARA A FUNÇÃO, O TAMANHO E O INDICE PARA A CRIAÇÃO DO ARQUIVO
-   cria_arquivo $tamanho $a
+   cria_arquivo $tamanho $a $caminho
    
    echo "Arquivo $a criado !"
 
    a=`expr $a + 1`
 done
-
 
 
 

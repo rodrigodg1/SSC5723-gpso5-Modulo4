04e36743d483f005369b70a963cae979e1447ea6
rodrigog10 <62249997+rodrigog10@users.noreply.github.com>
2020-06-19 11:16:04 -0300

    Update ler-arquivos.sh

--- ler-arquivos.sh
+++ ler-arquivos.sh
@@ -4,16 +4,29 @@
 #FUNÇÃO RESPOSÁVEL POR LER O ARQUIVO QUE RECEBE EM $1 E MOSTRAR O TEMPO DE LEITURA
 ler_arquivo() {
 
+
+   #VERIFICA SE O DIRETORIO PARA CRIACAO EH O ATUAL	
+
+		
+   echo "Diretorio sendo utilizado para LEITURA: $2"	
    sleep 2
    
-   #time -p cat arquivo$1.txt
-   strace -c cat arquivo$1.txt
+   
+   #realiza a leitura do arq 
+   strace -c cat $2/arquivo$1.txt
    
 }
 
 
 
 a=1
+
+   echo "\nDigite o Caminho do Sistema de Arquivo Destino"
+   echo "Exemplo: /media/rodrigo/FAT32/"
+   echo "ou"
+   echo ". para o Diretorio Atual"
+   read caminho
+
 
 
 #LOOP PARA A LEITURA DOS ARQUIVOS CRIADOS
@@ -22,17 +35,16 @@ do
    
 
    
-   echo "\n\nLendo o arquivo ($a/10)  ..."	
+   echo "\nLendo o arquivo ($a/10)  ..."	
    
    #CHAMA A FUNÇÃO PASSANDO O INDICE DO ARQUIVO QUE FOI CRIADO ANTERIORMENTE
-   ler_arquivo $a
+   ler_arquivo $a $caminho
    
    echo "Arquivo $a lido !"
 
 
    a=`expr $a + 1`
 done
-
 
 
 

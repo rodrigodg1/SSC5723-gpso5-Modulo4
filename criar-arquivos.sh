87b449d954312a23fd60552513f29e43dd5bd33b
rodrigog10 <62249997+rodrigog10@users.noreply.github.com>
2020-06-19 15:50:42 -0300

    Update criar-arquivos.sh

--- criar-arquivos.sh
+++ criar-arquivos.sh
@@ -16,27 +16,29 @@ cria_arquivo() {
 
 
 
-a=1
-
-   echo "Digite o Tamanho do Arquivo\n\n5KB \n10KB \n100KB \n1MB\n10MB \n100MB \n500MB
+#REALIZA A LEITURA DO TAM DO ARQUIVO, QUANTIDADE E O PATH DO SISTEMA DE ARQUIVO
+   echo "\nDigite o TAMANHO do Arquivo\n\n  5KB \n  10KB \n  100KB \n  1MB\n  10MB \n  100MB \n  500MB
 \n\nExemplo:1MB [ENTER]:"
    read tamanho
 
-   echo "\nDigite o Caminho do Sistema de Arquivo Destino:"
+   echo "\nInforme a QUANTIDADE de arquivos a serem CRIADOS para a analise:"
+   read quantidade
+
+   echo "\nDigite o CAMINHO do sistema de arquivo destino:"
    echo "Exemplo: /media/rodrigo/FAT32/"
-   echo "ou"
-   echo ". para o Diretorio Atual\n"
+   echo "ou . para o diretorio atual\n"
    read caminho
 
 
+a=1
 
 
 #LOOP PARA CRIAR 10 ARQUIVOS PARA TIRAR A MÉDIA DOS TEMPOS 
-while [ "$a" -lt 11 ]   
+while [ "$a" -lt `expr $quantidade + 1` ]   
 do
    
    
-   echo "\n\nCriando arquivo ($a/10)  ..."
+   echo "\n\nCriando arquivo ($a/$quantidade)  ..."
    
    #PASSA PARA A FUNÇÃO, O TAMANHO E O INDICE PARA A CRIAÇÃO DO ARQUIVO
    cria_arquivo $tamanho $a $caminho
